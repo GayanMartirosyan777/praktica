561cc289564bedbe46eda0681fa67b63c530ba9e
GayanMartirosyan777 <114398638+GayanMartirosyan777@users.noreply.github.com>
2023-01-17 15:40:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// 1
+// var minute = 28;
+// if (minute >= 0 && minute<=14){
+//     console.log("В первую четверть. ");
+// }
+// if (minute >= 15 && minute<=30){
+//     console.log("Во вторую четверть. ");
+// }
+// if (minute >= 31 && minute<=45){
+//     console.log("В третью четверть. ");
+// }
+// if (minute >= 46 && minute<=59){
+//     console.log("В четвертую четверть. ");
+// }
+
+// 5
+//  var i = 1
+//  while (i <= 12) {
+//     console.log(i);
+//     i++
+//   }
+
+// 6
+// var i = 5
+//  while (i <= 13) {
+//     console.log(i);
+//     i++
+//   }
+
+// 7
+//for (let i = 0; i <= 16; i++)
+//    if(i % 2 == 0) {
+//        console.log(i);
+//    }
+
+//8
+// let array = [2,5,8,3,6];
+// let result = 0;
+// for (let i = 0; i < array.length; i++) {
+//     result += array[i];
+// }
+// console.log(result);
+
+//2
+// let lang = "ru";
+// let arr = [];
+// if (lang == "ru"){
+//     arr = ["пн","вт","ср","чт","пт","сб","вс"]
+// }
+// if (lang == "en"){
+//     arr = ["mn","ts","wd","th","fr","st","sn"]
+  
+// }
+
+//   console.log(arr)
+
+// 4
+// let red = "да", yellow = 'нет';
+// if(red == "да"){
+//     console.log("Красный сигнал")
+// }
+// else if (yellow == "да") {
+//     console.log("Желтый сигнал")
+// }
+// else{
+//     console.log("Зеленый сигнал можно переходить дорогу");
+// }
